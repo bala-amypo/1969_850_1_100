74baf8aacf7d19c8970058fdf86c974060f69b22
bala-amypo <bala@amypo.in>
2025-12-20 03:42:29 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplianceRuleRepository.java
+++ src/main/java/com/example/demo/repository/ComplianceRuleRepository.java
@@ -1,18 +1,7 @@
-[9:05 AM, 12/20/2025] Sahana: package com.example.demo.repository;
+ package com.example.demo.repository;
 
 import com.example.demo.model.ComplianceRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComplianceRuleRepository extends JpaRepository<ComplianceRule, Long> {
-}
-[9:05 AM, 12/20/2025] Sahana: package com.example.demo.repository;
-
-import com.example.demo.model.ComplianceScore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ComplianceScoreRepository extends JpaRepository<ComplianceScore, Long> {
-
-    Optional<ComplianceScore> findByVendor_Id(Long vendorId);
 }
