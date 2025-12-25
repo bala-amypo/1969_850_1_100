615ae245767fa5a85b346b6be57553a50a642490
bala-amypo <bala@amypo.in>
2025-12-25 11:49:37 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
         @UniqueConstraint(columnNames = "email")
 })
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -43,16 +42,36 @@ public class User {
         }
     }
 
-    public Long getId() { return id; }
-    public String getFullName() { return fullName; }
-    public String getEmail() { return email; }
-    public String getPassword() { return password; }
-    public String getRole() { return role; }
+    public Long getId() {
+     return id; 
+     }
+    public String getFullName() { 
+    return fullName;
+     }
+    public String getEmail() {
+     return email;
+      }
+    public String getPassword() {
+     return password;
+      }
+    public String getRole() { 
+    return role; 
+    }
     public LocalDateTime getCreatedAt() { return createdAt; }
 
-    public void setId(Long id) { this.id = id; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
-    public void setEmail(String email) { this.email = email; }
-    public void setPassword(String password) { this.password = password; }
-    public void setRole(String role) { this.role = role; }
+    public void setId(Long id) {
+     this.id = id; 
+     }
+    public void setFullName(String fullName) {
+     this.fullName = fullName;
+      }
+    public void setEmail(String email) {
+     this.email = email;
+      }
+    public void setPassword(String password) { 
+    this.password = password; 
+    }
+    public void setRole(String role) { 
+    this.role = role;
+     }
 }
