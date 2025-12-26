ef25e32bb65c5a11aabca6fee57df654f63ba5f5
bala-amypo <bala@amypo.in>
2025-12-26 07:55:49 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,13 +1,15 @@
+package com.example.demo.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration  // marks this as a configuration class
+@Configuration
 public class SecurityConfig {
 
-    @Bean  // tells Spring "here is a bean you can inject anywhere"
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // standard password hashing algorithm
+        return new BCryptPasswordEncoder();
     }
 }
