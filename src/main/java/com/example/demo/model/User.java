587f4d8f544d38746f6a25c0596fca4b00bd116c
bala-amypo <bala@amypo.in>
2025-12-28 15:14:10 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,7 +1,5 @@
 package com.example.demo.model;
-
 import jakarta.persistence.*;
-
 @Entity
 @Table(
         name = "users",
@@ -21,18 +19,38 @@ public class User {
     private String password;
     private String role;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() { 
+        return id; 
+        }
+    public void setId(Long id) { 
+        this.id = id;
+         }
 
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getFullName() {
+         return fullName;
+          }
+    public void setFullName(String fullName) { 
+        this.fullName = fullName; 
+        }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+    return email;
+     }
+    public void setEmail(String email) { 
+    this.email = email;
+     }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() {
+         return password;
+          }
+    public void setPassword(String password) { 
+        this.password = password; 
+        }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public String getRole() { 
+        return role; 
+        }
+    public void setRole(String role) { 
+        this.role = role;
+         }
 }
