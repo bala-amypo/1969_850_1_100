a55382187117778eb7939a5b40574083cfa26dab
bala-amypo <bala@amypo.in>
2025-12-28 02:30:56 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
@@ -3,10 +3,13 @@ package com.example.demo.service.impl;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Vendor;
 import com.example.demo.repository.VendorRepository;
+import com.example.demo.service.VendorService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class VendorServiceImpl {
+public class VendorServiceImpl implements VendorService {
 
     private final VendorRepository vendorRepository;
 
@@ -14,13 +17,19 @@ public class VendorServiceImpl {
         this.vendorRepository = vendorRepository;
     }
 
+    @Override
     public Vendor createVendor(Vendor vendor) {
         return vendorRepository.save(vendor);
     }
 
+    @Override
     public Vendor getVendor(Long id) {
         return vendorRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Vendor not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Vendor not found"));
+    }
+
+    @Override
+    public List<Vendor> getAllVendors() {
+        return vendorRepository.findAll();
     }
 }
