9995af2fbfed48d819835b7b63bbd3b77f5ceea8
bala-amypo <bala@amypo.in>
2025-12-30 03:51:16 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -5,12 +5,24 @@ public class RegisterRequest {
     private String email;
     private String password;
 
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getFullName() { 
+        return fullName; 
+        }
+    public void setFullName(String fullName) {
+         this.fullName = fullName;
+          }
 	
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+        return email; 
+        }
+    public void setEmail(String email) {
+         this.email = email;
+          }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() {
+         return password;
+          }
+    public void setPassword(String password) {
+         this.password = password;
+          }
 }
